ee185a7ac523d35a90569dd0421b309b578c2ba9
cavazzo <js.cavazos95@gmail.com>
2022-09-20 22:55:03 -0500

    Added missing import

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,7 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
+import partytown from "@astrojs/partytown";
 
 import image from "@astrojs/image";
 
